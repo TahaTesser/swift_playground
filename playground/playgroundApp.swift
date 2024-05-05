811b96036c786fac1664a401716fac74e6faf170
Taha Tesser <tessertaha@gmail.com>
2024-05-05 23:39:39 +0300

    Setup

---
+++ playground/playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  playgroundApp.swift
+//  playground
+//
+//  Created by Taha Tesser on 05.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
